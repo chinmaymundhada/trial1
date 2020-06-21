83022e264f078b4ff0f19c5085439ed9d4d3d54e
chinmay0208 <mundhadachinmay@gmail.com>
2020-06-21 14:54:36 +0530

    Intial Commit
    The bot is just greeting the user

---
+++ bot.js
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+
+class EchoBot extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async (context, next) => {
+            const replyText = `Echo: ${ context.activity.text }`;
+            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            await this.sendWelcomeMessage(context)
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+
+    async sendWelcomeMessage(turnContext) {
+        const { activity } = turnContext;
+
+        // Iterate over all new members added to the conversation.
+        for (const idx in activity.membersAdded) {
+            if (activity.membersAdded[idx].id !== activity.recipient.id) {
+                const welcomeMessage = `Happy Thoughts!! Welcome to Bot Service ${ activity.membersAdded[idx].name }. `;
+                await turnContext.sendActivity(welcomeMessage);
+                await this.sendSuggestedActions(turnContext);
+            }
+        }
+    }
+
+    async sendSuggestedActions(turnContext) {
+        var reply = MessageFactory.suggestedActions(['English', 'Hindi'], 'Which language would you prefer?');
+        await turnContext.sendActivity(reply);
+    }
+}
+
+module.exports.EchoBot = EchoBot;
